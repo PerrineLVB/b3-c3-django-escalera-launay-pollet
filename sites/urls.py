fb84ed00183873dbb96f1c3390336df9a961a0d1
MathieuCoding <polletmathieu.pro@gmail.com>
2024-01-09 18:49:42 +0100

    urls updated

---
+++ sites/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create_site/', views.create_site, name='create_site'),
+    path('update_site/<int:pk>/', views.update_site, name='update_site'),
+]
