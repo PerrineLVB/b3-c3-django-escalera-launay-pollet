e094609a8330484cdc3d1abafc6bbb206565617c
MathieuCoding <polletmathieu.pro@gmail.com>
2024-01-19 13:43:19 +0100

    csv export done

--- sites/urls.py
+++ sites/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create_site/', views.create_site, name='create_site'),
     path('update_site/<int:pk>/', views.update_site, name='update_site'),
+    path('export_csv/', views.export_csv, name='export_csv')
+
 ]
