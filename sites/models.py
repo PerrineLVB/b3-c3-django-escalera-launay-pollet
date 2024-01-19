9da63c09b721d2f4a46d181de9ca0d52cd05d75e
Perrine Launay <plaunay@medinplus.com>
2024-01-19 18:23:03 +0100

    trying to make deletion prettier - WIP

--- sites/models.py
+++ sites/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 # Create your models here.
 class Site(models.Model):
     name = models.CharField(max_length=255)
@@ -8,3 +9,6 @@ class Site(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('site_details', args=[str(self.id)])
