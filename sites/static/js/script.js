6b90f525f074d982464a0f6f37777523f99fd6d5
MathieuCoding <polletmathieu.pro@gmail.com>
2024-02-13 14:04:46 +0100

    dark mode done

--- sites/static/js/script.js
+++ sites/static/js/script.js
@@ -8,3 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+document.getElementById('darkModeToggle').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark')
+    }
+})
