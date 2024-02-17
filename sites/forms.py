f6923291c29ee8f59e9fcb28480b55205c449a2b
Alice Escalera <alice.escalera@gmail.com>
2024-02-17 11:56:46 +0100

    add error message to register

--- sites/forms.py
+++ sites/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+#translating the form in french
+from django.utils.translation import gettext_lazy as _
 
 from .models import Site
 
@@ -14,6 +16,13 @@ class SiteForm(forms.ModelForm):
 
 
 class UserRegistrationForm(UserCreationForm):
+    error_messages = {
+        'password_mismatch': _("Les deux mots de passe ne correspondent pas."),
+        'username_exists': _("Ce nom d'utilisateur existe déjà."),
+    }
+    username = forms.CharField(label=_("Nom d'utilisateur"))
+    password1 = forms.CharField(label=_("Mot de passe"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Confirmation du mot de passe"), widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
